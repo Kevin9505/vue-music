f99160ba33e8ee22acf1226ec0127c89c3a2ed2d
likaifeng <1980942486@qq.com>
2019-05-31 22:23:44 +0800

    2019-5-31 四大分栏。

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    排行榜
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    评论页
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
