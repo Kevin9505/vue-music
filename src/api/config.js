d449dcc8cebc6db0e40953c4fbbff25c151aaff1
likaifeng <1980942486@qq.com>
2019-06-04 22:08:43 +0800

    2019-6-4 封装一个jsonp发请求的方法。

---
+++ src/api/config.js
@@ -0,0 +1,16 @@
+/**
+ * 一些常用的配置
+ */
+
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'utf8',
+  outCharset: 'utf8',
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
