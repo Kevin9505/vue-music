09a455395a60a1d8dcc25e57efb32a4787983ed5
likaifeng <1980942486@qq.com>
2019-06-04 22:10:15 +0800

    2019-6-4 推荐页面成功获取QQ音乐的轮播图数据，实现轮播。

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    评论页
+  <div class="recommend">
+    <slider v-if="recommends.length">
+      <div v-for="item in recommends" :key="item.id">
+        <a :href="item.linkUrl">
+          <img :src="item.picUrl" :alt="item.id">
+        </a>
+      </div>
+    </slider>
   </div>
 </template>
 
 <script>
+import { getRecommend } from '@/api/recommend'
+import { ERR_OK } from '@/api/config'
+import Slider from '@/base/slider/slider'
 export default {
-  
+  name: 'recommend',
+
+  data() {
+    return {
+      recommends: []
+    }
+  },
+
+  created() {
+    this._getRecommend()
+  },
+
+  methods: {
+    _getRecommend () {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          const {slider} = res.data
+          this.recommends = slider
+        }
+      })
+    }
+  },
+
+  components: {
+    Slider
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '../../common/stylus/variable'
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
 </style>
