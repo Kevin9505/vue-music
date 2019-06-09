36b3f4746c1e84d7d0a2adb838d805dd46027428
likaifeng <1980942486@qq.com>
2019-06-09 23:12:08 +0800

    2019-6-9 添加 vue-lazyloading 插件实现图片懒加载。

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import fastclick from 'fastclick'
 import App from './App.vue'
 import router from './router'
+import VueLazyload from 'vue-lazyload'
 
 import '@/common/stylus/index.styl'
 
 // 解决移动端点击延迟 300 ms 的
 fastclick.attach(document.body)
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('@/common/images/default.png')
+})
 
 new Vue({
   render: h => h(App),
