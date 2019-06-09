5015a07ee571d13e7167c278a72dd7d8bd5e837c
likaifeng <1980942486@qq.com>
2019-06-09 23:10:42 +0800

    2019-6-9 发请求时，数据未回来时的提示。

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading">
+    <img height="24" width="24" src="./loading.gif" alt="loading">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loading',
+
+  props: {
+    title: {
+      type: String,
+      default: '正在加载...'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../../common/stylus/variable.styl"
+.loading
+  width: 100%
+  text-align: center
+  .desc
+    line-height: 20px
+    font-size: $font-size-small
+    color: $color-text-l
+</style>
+
