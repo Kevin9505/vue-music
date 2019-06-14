c98e1f3251bf3b141b67dee3feebdd98abff96b5
likaifeng <1980942486@qq.com>
2019-06-14 23:51:10 +0800

    2019-6-14 添加滚动到指定的位置和滚动到指定的目标元素的方法。

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -6,7 +6,6 @@
 
 <script>
 import BScroll from 'better-scroll'
-import { setTimeout } from 'timers';
 export default {
   name: 'scroll',
 
@@ -36,24 +35,59 @@ export default {
   },
 
   methods: {
-    _initScroll() {
-      if (!this.$refs.wrapper) return
-      this.scroll = new BScroll(this.$refs.wrapper, {
-        probeType: this.probeType,
-        click: this.click
-      })
-    },
-
+    /**
+     * 禁用 better-scroll 的DOM 事件(如 touchstart,touchmove,touchend)的回调函数不再响应
+     */
     disable() {
       this.scroll && this.scroll.disable()
     },
 
+    /**
+     * 启用 better-scroll, 默认 开启
+     */
     enable() {
       this.scroll && this.scroll.enable()
     },
 
+    /**
+     * 重新计算 better-scroll,当 DOM 结构发生变化的时候务必要调用确保滚动的效果正常
+     */
     refresh() {
       this.scroll && this.scroll.refresh()
+    },
+
+    /**
+     * 滚动到指定的位置
+     */
+    scrollTo() {
+      this.scroll && this.scrollTo.apply(this.scroll, arguments)
+    },
+
+    /**
+     * 滚动到指定的目标元素
+     */
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+    },
+    
+    /**
+     * 初始化better-scroll
+     */
+    _initScroll() {
+      if (!this.$refs.wrapper) return
+      // 初始化 scroll 
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+      // 是否监听滚动
+      if (this.listenScroll) {
+        let _me = this
+        this.scroll.on('scroll', (pos) => {
+          // 监听滚动,派发滚动事件
+          _me.$emit('scroll', pos)
+        })
+      }
     }
   },
 
