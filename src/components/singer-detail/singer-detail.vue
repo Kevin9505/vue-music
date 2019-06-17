6416d90b039356f3f9a74a5cafd41f39536d3635
likaifeng <1980942486@qq.com>
2019-06-17 23:39:11 +0800

    2019-6-17 歌手的歌曲详情页面的实现。

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,42 +1,89 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-      歌手详情页
-    </div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script>
+import MusicList from '@/components/music-list/music-list'
 import {mapGetters} from 'vuex'
+import {getSingerDetail} from '@/api/singer'
+import {ERR_OK} from '@/api/config'
+import {createSong} from '@/common/js/song'
 
 export default {
   name: 'singer-detail',
+
+  data() {
+    return {
+      songs: []
+    }
+  },
   
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
   },
 
   created() {
-    console.log(this.singer)
+    this._getSingerDetail(this.singer.id)
   },
+
+  methods: {
+    /**
+     * 一个获取歌手详情的方法
+     * @param {*} 歌手 Id
+     */
+    _getSingerDetail(id) {
+      // 当取不到歌手 ID 时返回上一页
+      if (!this.singer.id) {
+        this.$router.back()
+        return
+      }
+      // 获取歌手数据
+      getSingerDetail(id).then((res) => {
+        console.log(res)
+        if (res.code === ERR_OK) {
+          const {list} = res.data
+          this.songs = this._normalizeSinger(list)
+        }
+      })
+    },
+
+    /**
+     * 一个格式化歌曲数据格式的方法
+     * @param {*} list 歌手的歌曲数据
+     */
+    _normalizeSinger(list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        // 当有歌曲 ID 和 专辑 ID 
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  },
+
+  components: {
+    MusicList
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '../../common/stylus/variable.styl'
 .slide-enter-active, .slide-leave-active
   transition: all 0.3s
 
 .slide-enter, .slide-leave-to
   transform: translate3d(100%, 0, 0)
-.singer-detail
-  position: fixed
-  top: 0
-  right: 0
-  bottom: 0
-  left: 0
-  background: $color-background
-  z-index: 100
 </style>
