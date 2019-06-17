52df36e651bde5e61d1f700667c952d47e69b329
likaifeng <1980942486@qq.com>
2019-06-17 23:37:24 +0800

    2019-6-17 封装一个公共的歌曲列表组件。

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="item" v-for="item in songs" :key="item.id">
+        <div class="rank" v-show="rank">
+          <span class=""></span>
+        </div>
+        <div class="content">
+          <h2 class="name">{{item.name}}</h2>
+          <p class="desc">{{getDesc(item)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'song-list',
+
+  props: {
+    songs: {
+      type: Array,
+      // eslint-disable-next-line vue/require-valid-default-prop
+      default: []
+    },
+    rank: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    getDesc(song) {
+      return `${song.singer} · ${song.album}`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../common/stylus/variable'
+@import '../../common/stylus/mixin'
+.song-list
+  .item
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    height: 64px
+    font-size: $font-size-medium
+    .rank
+      display: flex
+      width: 25px
+      margin-right: 30px
+      text-align: center
+      .icon
+        display: inline-block
+        width: 25px
+        height: 24px
+        background-size: 25px 24px
+        &.icon0
+          bg-image('first')
+        &.icon1
+          bg-image('second')
+        &.icon2
+          bg-image('third')
+      .text
+        color: $color-theme
+        font-size: $font-size-large
+    .content
+      flex: 1
+      line-height: 20px
+      overflow: hidden
+      .name
+        no-wrap()
+        color: $color-text
+      .desc
+        no-wrap()
+        margin-top: 4px
+        color: $color-text-d
+</style>
+
